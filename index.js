d791b47a7c81124a0e8fcbe1363c853cd6cbd410
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-29 11:43:47 -0300

    up-project

---
+++ index.js
@@ -0,0 +1,9 @@
+//importar redux
+const redux = require('redux');
+const createStore = redux.createStore;
+
+//1º Definir a ação (para exemplo de contador)
+
+const increment = {type:'INCREMENT'}
+const decrement = {type:'DECREMENT'}
+
