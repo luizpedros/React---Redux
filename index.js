2a0562a8b5f1974df104ea61f9faad55971ba1ad
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-29 12:10:07 -0300

    up

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const createStore = redux.createStore;
 
 //1º Definir a ação (para exemplo de contador)
 
-const increment = {type:'INCREMENT'}
-const decrement = {type:'DECREMENT'}
+const incrementAction = {type:'INCREMENT'}
+const decrementAction = {type:'DECREMENT'}
+
+// 2º A ação depende do reducer, pois ele leva a o pedido que a acão quer fazer
+//O que o reducer precisa fazer vai depender de qual ação foi chamada.
+
+
+//caso a ação seja increment some 1 ou caso seja decrement subtraia 1
+function counterReducer(state=0, action){
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            state;
+    }
+}
+
+//3º Criar store para passar o status daa ação para Store amazenar esse status da ação
+
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{console.log(store.getState())})
+
 
